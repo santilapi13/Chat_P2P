26069be91450a3efd47f24958fb9c46d3835f744
santilapi13 <santilapiana02@gmail.com>
2023-04-30 18:55:07 -0300

    Documentacion clase Usuario.

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -10,6 +10,10 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
+/**
+ * @author : Grupo 4 - Avalos, Lapiana y Sosa
+ */
+
 public class Usuario implements Runnable, GestorSesiones, EnvioMensajes, GestorConexion {
     private Informacion informacion;
     private Socket socket;
@@ -76,7 +80,7 @@ public class Usuario implements Runnable, GestorSesiones, EnvioMensajes, GestorC
     }
 
     /**
-     * Se pone en modo escucha esperando una solicitud de chat.
+     * Se pone, concurrentemente, en modo escucha esperando una solicitud de chat.
      */
     @Override
     public void run() {
@@ -86,6 +90,15 @@ public class Usuario implements Runnable, GestorSesiones, EnvioMensajes, GestorC
         }
     }
 
+    /**
+     * Inicia los streams de entrada y salida del socket. Envia el username al receptor y recibe el username del usuario remoto.
+     * Instancia una nueva sesion, asignandola al atributo sesionActual, con la IP, puerto y username del usuario remoto.
+     * En caso de que sea el solicitante de la sesion de chat, invoca la creacion de la ventana de chat.<br>
+     * <b>Pre:</b> El socket debe estar conectado al usuario remoto.<br>
+     * <b>Post:</b> Se ha iniciado los streams de entrada y salida del socket. Se ha instanciado una nueva sesion con la IP, puerto y username del usuario remoto.
+     *
+     * @throws IOException: Si hay un error al iniciar los streams de entrada y salida del socket.
+     */
     private void iniciarESSockets() throws IOException {
         this.entradaSocket = new InputStreamReader(socket.getInputStream());
         this.entrada = new BufferedReader(entradaSocket);
@@ -102,6 +115,12 @@ public class Usuario implements Runnable, GestorSesiones, EnvioMensajes, GestorC
         }
     }
 
+    /**
+     * Activa el modo escucha del usuario. Se pone a la espera de una solicitud de chat. Cuando la recibe, crea un nuevo socket con el usuario remoto.<br>
+     * <b>Pre:</b> -<br>
+     * <b>Post:</b> Se ha creado un nuevo socket con el usuario remoto a partir del socketServer.
+     * @throws IOException: Si hay un error al crear el socket.
+     */
     private void activarModoEscucha() throws IOException {
         System.out.println("Modo escucha activado.");
         this.socketServer = new ServerSocket(this.getPuerto());
@@ -113,6 +132,10 @@ public class Usuario implements Runnable, GestorSesiones, EnvioMensajes, GestorC
         this.iniciarESSockets();
     }
 
+    /**
+     * Desactiva el modo escucha del usuario. Cierra el socketServer y el socket.<br>
+     * @throws IOException: Si hay un error al cerrar el socketServer.
+     */
     public void desactivarModoEscucha() throws IOException {
         this.socketServer.close();
         this.socketServer = null;
@@ -121,23 +144,44 @@ public class Usuario implements Runnable, GestorSesiones, EnvioMensajes, GestorC
         System.out.println("Modo escucha desactivado.");
     }
 
+    /**
+     * Solicita un chat al usuario remoto. Crea un nuevo socket con el usuario remoto, en caso de que su IP y puerto sean validos.<br>
+     * <b>Pre:</b> Los parametros IP y puerto deben ser validos.<br>
+     * <b>Post:</b> Se ha creado un nuevo socket con el usuario remoto.
+     * @param informacionReceptor: La informacion del usuario remoto.
+     * @throws IOException: Si hay un error al crear el socket.
+     */
     public void solicitarChat(Informacion informacionReceptor) throws IOException {
         this.socket = new Socket(informacionReceptor.getIP(),informacionReceptor.getPuerto());
         this.solicitando = 1;
         iniciarESSockets();
     }
 
+    /**
+     * Envia un mensaje al usuario remoto.<br>
+     * @param mensaje: El mensaje a enviar.
+     * @throws IOException: Si hay un error al enviar el mensaje.
+     */
     public void enviarMensaje(String mensaje) throws IOException {
         this.sesionActual.addMensaje(mensaje, true);
         this.salida.println(mensaje);
     }
 
+    /**
+     * Recibe un mensaje del usuario remoto.<br>
+     * @return El mensaje recibido.
+     * @throws IOException: Si hay un error al leer el mensaje.
+     */
     public String recibirMensaje() throws IOException {
         String mensaje = this.entrada.readLine();
         this.sesionActual.addMensaje(mensaje, false);
         return mensaje;
     }
 
+    /**
+     * Desconecta el socket del usuario remoto. Cierra los streams de entrada y salida del socket.
+     * @throws IOException: Si hay un error al cerrar los streams de entrada y salida del socket.
+     */
     public void desconectar() throws IOException {
         this.addNuevaSesion(this.sesionActual);
         sesionActual = null;
